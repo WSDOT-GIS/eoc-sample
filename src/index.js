ef5c3c71cbebdf0387a552566a1865ac20d2e43e
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2020-11-09 14:27:04 -0800

    added form and submit event

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import { RouteLocator } from "wsdot-elc";
 
 const routeLocator = new RouteLocator();
 
+// Since there's only one form on the page, we know 
+// it will be the first (#0) form.
+const theForm = document.forms.item(0);
+
+// Create event handler for form submit event.
+theForm.addEventListener("submit", function (e) {
+
+    try {
+        const route = document.getElementById("routeSelector").value
+        console.log(route);
+    }
+    catch (err) {
+        console.error(err);
+    }
+
+
+    // Stop the default behavior which would reload the page.
+    e.preventDefault();
+});
+
+// Query ELC for list of routes and then populate
+// the route select control.
 routeLocator.getRouteList(true).then(routeList => {
     // Get current LRS route list
     let routes = routeList.Current;
